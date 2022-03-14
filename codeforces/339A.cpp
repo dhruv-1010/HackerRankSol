12fc5aa80446dcae11561b06035b6401b327b740
Dhruv Singh <96680040+dhruv-1010@users.noreply.github.com>
2022-03-14 19:26:14 +0530

    Add files via upload

---
+++ codeforces/339A.cpp
@@ -0,0 +1,11 @@
+//339 A Sorting Strings a really nice questions i would definatly see 
+//editorial
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    int t,count=0;
+    cin>>t;
+    cout<<count<<endl;
+    return 0;
+}
---
+++ codeforces/50A.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    int t,count;
+    cin>>t>>count;
+    cout<<(int)(count*t)/2)<<endl;
+    return 0;
+}
